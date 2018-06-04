6899279b40968713cc70369e77f0bb3fd7a34b97
EdwinLovo <00120617@uca.edu.sv>
2018-06-03 18:21:15 -0600

    Iniciando estructura base de Revolucionario

---
+++ src/Revolucionario/FactoryRevolucionario.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Revolucionario;
+
+import AbstractFactory.AbstractFactory;
+import Militar.Militar;
+import Terrorista.Terrorista;
+
+/**
+ *
+ * @author EdwiLovo
+ */
+public class FactoryRevolucionario implements AbstractFactory{
+
+    @Override
+    public Militar getMilitar(String type) {
+        return null;
+    }
+
+    @Override
+    public Revolucionario getRevolucionario(String type) {
+        switch (type){
+            case "bitcoins":
+                return new BitCoins();
+            case "metal":
+                return new Metal();
+            case "plata":
+                return new Plata();
+            case "helicoptero":
+                return new Helicoptero();
+            case "moto":
+                return new Moto();
+            case "batallon":
+                return new Batallon();
+            case "castro":
+                return new Castro();
+        }
+        return null;
+    }
+
+    @Override
+    public Terrorista getTerrorista(String type) {
+        return null;
+    }
+    
+}
---
+++ src/Revolucionario/Revolucionario.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Revolucionario;
+
+/**
+ *
+ * @author EdwinLovo
+ */
+public interface Revolucionario {
+    public void atacar();
+    public void defender();
+    public void recolectar();
+    public void crear();
+}
