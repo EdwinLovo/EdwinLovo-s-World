1d5643a6dd4197f1378e8da8a4eef6db6e4177e5
EdwinLovo <00120617@uca.edu.sv>
2018-06-05 15:10:00 -0600

    Creando paquete para patron singleton y creando el primero de ellos

---
+++ src/Singletons/Fase.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Singletons;
+
+/**
+ *
+ * @author EdwinLovo
+ */
+public class Fase {
+    private static Fase instance;
+    private static int fase;
+
+    public Fase() {
+    }
+    
+    public static Fase getInstance(){
+        if(instance==null){
+            instance= new Fase();
+        }
+        else{
+            System.out.println("Objeto ya existente");
+        }
+        return instance;
+    }
+
+    public static int getFase() {
+        return fase;
+    }
+
+    public static void setFase(int fase) {
+        Fase.fase = fase;
+    }
+    
+    
+}
